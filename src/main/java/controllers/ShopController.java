6025fc1e8e94bc8d509c2ddec18b7f7581abc174
Angus Malloch <angusbass@hotmail.com>
2018-03-26 15:34:43 +0100

    Added edit_clothing.vtl  Added
    \q

--- src/main/java/controllers/ShopController.java
+++ src/main/java/controllers/ShopController.java
@@ -111,18 +111,18 @@ public class ShopController {
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 //
-//        get("/shops/bike/:id/edit", (req, res) -> {
-//            String strId = req.params(":id");
-//            Integer intId = Integer.parseInt(strId);
-//            Bike bike = DBHelper.find(intId, Bike.class);
-//            Map<String, Object> model = new HashMap<>();
-//            model.put("bike", bike);
-//            model.put("template", "templates/shops/edit_bike.vtl");
-//            return new ModelAndView(model, "templates/layout.vtl");
-//        }, new VelocityTemplateEngine());
+        get("/shops/clothing/:id/edit", (req, res) -> {
+            String strId = req.params(":id");
+            Integer intId = Integer.parseInt(strId);
+            Clothing clothing = DBHelper.find(intId, Clothing.class);
+            Map<String, Object> model = new HashMap<>();
+            List<ClothingType> clothes = DBHelper.getClothingType();
+            model.put("clothes", clothes);
+            model.put("clothing", clothing);
+            model.put("template", "templates/shops/edit_clothing.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
 
-//        String name, double price, int quantity, String colour, String size, ClothingType type
-//
         post ("/shops/clothing", (req, res) -> {
             String name = req.queryParams("name");
             Double price = Double.parseDouble(req.queryParams("price"));
@@ -137,29 +137,29 @@ public class ShopController {
             return null;
         }, new VelocityTemplateEngine());
 
-//
-//        post ("/shops/bike/:id", (req, res) -> {
-//            String strId = req.params(":id");
-//            Integer intId = Integer.parseInt(strId);
-//            Bike bike = DBHelper.find(intId, Bike.class);
-//            String name = req.queryParams("name");
-//            Double price = Double.parseDouble(req.queryParams("price"));
-//            int quantity = Integer.parseInt(req.queryParams("quantity"));
-//            String colour = req.queryParams("colour");
-//            int capacity = Integer.parseInt(req.queryParams("capacity"));
-//            Boolean isNew = Boolean.parseBoolean(req.queryParams("isNew"));
-//            bike.setName(name);
-//            bike.setPrice(price);
-//            bike.setQuantity(quantity);
-//            bike.setColour(colour);
-//            bike.setCapacity(capacity);
-//            bike.setNew(isNew);
-//            DBHelper.update(bike);
-//            res.redirect("/shops/stock");
-//            return null;
-//
-//        }, new VelocityTemplateEngine());
-//
+        post ("/shops/clothing/:id", (req, res) -> {
+            String strId = req.params(":id");
+            Integer intId = Integer.parseInt(strId);
+            Clothing clothing = DBHelper.find(intId, Bike.class);
+            String name = req.queryParams("name");
+            Double price = Double.parseDouble(req.queryParams("price"));
+            int quantity = Integer.parseInt(req.queryParams("quantity"));
+            String colour = req.queryParams("colour");
+            String size = req.queryParams("size");
+            String stringType = req.queryParams("type");
+            ClothingType type = ClothingType.valueOf(stringType);
+            clothing.setName(name);
+            clothing.setPrice(price);
+            clothing.setQuantity(quantity);
+            clothing.setColour(colour);
+            clothing.setSize(size);
+            clothing.setType(type);
+            DBHelper.update(clothing);
+            res.redirect("/shops/stock");
+            return null;
+
+        }, new VelocityTemplateEngine());
+
 //        post ("/shops/bike/:id/delete", (req, res) -> {
 //            int id = Integer.parseInt(req.params(":id"));
 //            Bike bikeToDelete = DBHelper.find(id, Bike.class);
